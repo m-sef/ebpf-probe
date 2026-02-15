346aff05965059f747c542f9225861479a246a6c
Seth Moore <seth.l.moore0506@gmail.com>
2026-02-15 15:01:57 -0500

    Added init_logger function (though no logging yet), as well as kernelspace variables

--- packet-counter.bpf.c
+++ packet-counter.bpf.c
@@ -1,15 +1,25 @@
 #include <uapi/linux/bpf.h>
 
-BPF_HASH(packet_counter, u64);
-BPF_HASH(byte_counter, u64);
+BPF_ARRAY(packets, u64, 1);
+BPF_ARRAY(bytes, u64, 1);
 
 int xdp_prog(struct xdp_md *ctx)
 {
     u32 packet_size = ctx->data_end - ctx->data;
-    //bpf_trace_printk("packet size:%d\n", packet_size);
 
-    packet_counter.increment(1);
-    byte_counter.increment(packet_size);
+    u32 key = 0;
+
+    u64* pkt = packets.lookup(&key);
+    if (pkt)
+    {
+        (*pkt)++;
+    }
+
+    u64* byt = bytes.lookup(&key);
+    if (byt)
+    {
+        (*byt) += packet_size;
+    }
 
     return XDP_PASS;
 }
--- packet-counter.py
+++ packet-counter.py
@@ -1,23 +1,56 @@
 #!/user/bin/env python3
+import sys
+import socket
+import logging
+from logging import Logger
+from datetime import datetime
 import time
 from bcc import BPF
 from pathlib import Path
 
+def init_logger(
+        name : str,
+        file_path : str,
+        level=logging.INFO,
+        date_format : str = "%Y-%m-%d %H:%M:%S") -> Logger:
+    """Function for creating new Logger objects
+    https://stackoverflow.com/questions/11232230/logging-to-two-files-with-different-settings"""
+
+    formatter = logging.Formatter(
+        '%(asctime)s,%(message)s',
+        datefmt=date_format)
+
+    handler = logging.FileHandler(file_path)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
 def main() -> None:
-    interface = 'enp0s31f6'
+    interface = sys.argv[1]
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    hostname = socket.gethostname().split('.')[0]
+
+    #packet_counter_logger = init_logger(
+    #    "rx",
+    #    f"{hostname}-{interface}-{timestamp}.log")
+
     src = Path('packet-counter.bpf.c').read_text()
     bpf = BPF(text=src)
     function = bpf.load_func("xdp_prog", BPF.XDP)
 
     bpf.attach_xdp(interface, function, 0)
 
-    packet_counter = bpf["packet_counter"]
-    byte_counter = bpf["byte_counter"]
+    print("packets,bytes")
 
     try:
         while True:
             time.sleep(1)
 
+            print(bpf['packets'][0].value, bpf['bytes'][0].value, sep=',');
     except KeyboardInterrupt:
         pass
 
@@ -25,5 +58,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-#3179959 le ticket number
