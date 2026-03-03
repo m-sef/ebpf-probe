f29775073418399fd8d292438484fc8094da1eec
m-sef <seth.l.moore0506@gmail.com>
2026-03-02 19:57:07 -0500

    Removed Weverything and Werror flags from clang compile command, starting to work on implementing skeleton generation in CMakeLists.txt

--- src/main.c
+++ src/main.c
@@ -1,27 +1,38 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <net/if.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 
 #define REQUIRED_ARGS \
-    REQUIRED_STRING_ARG(interface, "interface", "Interface name")
+	REQUIRED_STRING_ARG(interface_name, "interface", "Interface name")
 
 #define BOOLEAN_ARGS \
-    BOOLEAN_ARG(help, "-h", "Show help")
+	BOOLEAN_ARG(verbose, "-v", "Enable verbose output") \
+	BOOLEAN_ARG(help, "-h", "Show help")
 
 #include "easyargs.h"
 
 int main(int argc, char** argv)
 {
-    args_t args = make_default_args();
-    int spongebob = 0;
+	args_t args = make_default_args();
 
-    if (!parse_args(argc, argv, &args) || args.help)
-    {
-        print_help(argv[0]);
+	if (!parse_args(argc, argv, &args) || args.help)
+	{
+		print_help(argv[0]);
 
-        return EXIT_FAILURE;
-    }
+		return EXIT_FAILURE;
+	}
 
-    printf("Vile Machinations!!!\n");
+	unsigned int interface_index = if_nametoindex(args.interface_name);
+	if (!interface_index)
+	{
+		printf("Could not find interface \"%s\"\n", args.interface_name);
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	printf("Interface Index: %d\n", interface_index);
+
+	return EXIT_SUCCESS;
 }
--- src/xdp_prog.bpf.c
+++ src/xdp_prog.bpf.c
@@ -2,9 +2,9 @@
 #include <bpf/bpf_helpers.h>
 
 SEC("xdp")
-static int xdp_prog(struct xdp_md* context)
+int xdp_prog(struct xdp_md* context)
 {
-    bpf_trace_printk("Packet Size: %d", context->data_end - context->data);
+	bpf_trace_printk("Packet Size: %d", context->data_end - context->data);
 
-    return XDP_PASS;
+	return XDP_PASS;
 }
