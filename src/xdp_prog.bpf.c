82eed2428fe7f1cef7f489a24641ba57f8080d61
Seth Moore <seth.l.moore0506@gmail.com>
2026-03-04 18:00:19 -0500

    Implemented vmlinux.h generation via CMake, still working on transferring data between kernel-space and user-space

--- src/xdp_prog.bpf.c
+++ src/xdp_prog.bpf.c
@@ -1,14 +1,13 @@
-#include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
+//#include <linux/bpf.h>
+//#include <linux/if_ether.h>
+//#include <linux/ip.h>
+//#include <linux/udp.h>
 //#include <linux/icmp.h>
-#include <linux/in.h>
+//#include <linux/in.h>
 
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
-
-#include "icmp.h"
 
 enum direction_t { SOURCE, DESTINATION };
 
@@ -52,21 +51,21 @@ int xdp_prog(struct xdp_md* context)
 		return XDP_PASS;
 
 	/* Only handling IPv4 packets */
-	if (ethernet_header->h_proto != ETH_P_IP)
-		return XDP_PASS;
+	//if (ethernet_header->h_proto != ETH_P_IP)
+	//	return XDP_PASS;
 
 	struct iphdr* ipv4_header = data + sizeof(*ethernet_header);
 	if ((void*)ipv4_header + sizeof(*ipv4_header) > data_end)
 		return XDP_PASS;
 
-	/* if (ip_header->protocol != IPPROTO_ICMP)
+	if (ipv4_header->protocol != IPPROTO_ICMP)
 		return XDP_PASS;
 
-	struct icmphdr* icmp_header = (void*)ip_header + sizeof(*ip_header);
+	struct icmphdr* icmp_header = (void*)ipv4_header + sizeof(*ipv4_header);
 	if ((void*)icmp_header + sizeof(*icmp_header) > data_end)
 		return XDP_PASS;
 
-	int payload_size = bpf_ntohs(ip_header->tot_len) - sizeof(*ip_header) - sizeof(*icmp_header); */
+	int payload_size = bpf_ntohs(ipv4_header->tot_len) - sizeof(*ipv4_header) - sizeof(*icmp_header);
 
 	struct key_t temp_key = {0};
 	struct value_t temp_value = {0};
