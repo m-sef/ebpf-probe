cc5fdd2f9df1359371bca045a83e327436c8da9e
Seth Moore <seth.l.moore0506@gmail.com>
2026-03-21 22:21:31 -0400

    Fixed graphing script

--- scripts/graph.py
+++ scripts/graph.py
@@ -5,25 +5,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def main() -> None:
-    file_path : str = "build/test.log"
+    file_path : str = "build/test.csv"
 
     data_frame = pd.read_csv(file_path, comment='#')
-    data_frame = data_frame.groupby('timestamp')[['total_packets_recieved', 'total_bytes_recieved']].sum().reset_index()
+
+    data_frame['datetime'] = pd.to_datetime(data_frame['time'], unit='ns')
+    data_frame = data_frame.set_index('datetime')
+
+    data_frame = data_frame[['size']].resample('1s').sum()
+    data_frame['size'] = data_frame['size'] / 1024
 
     plt.plot(
-        pd.to_datetime(data_frame['timestamp'], format="%Y-%m-%d %H:%M:%S"),
-        data_frame['total_packets_recieved'].diff())
+        data_frame.index,
+        data_frame['size'],
+        linewidth=1.0)
 
     plt.title(file_path)
     plt.xlabel("Time")
-    plt.ylabel("Throughput (Bps)")
+    plt.ylabel("Throughput (Kilobytes/sec)")
 
     plt.ticklabel_format(
         useOffset=False,
         style='plain',
         axis='y')
 
-    plt.grid()
+    plt.grid(
+        True,
+        linestyle='--',
+        alpha=0.5)
 
     plt.show()
 
