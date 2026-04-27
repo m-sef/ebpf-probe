6aa2e0a13742343d7269bc8ba5ba8072d72069ba
m-sef <seth.l.moore0506@gmail.com>
2026-04-26 21:45:47 -0400

    Add perf event counter readout to BPF iterator output

--- src/ebpf_probe_iter.bpf.c
+++ src/ebpf_probe_iter.bpf.c
@@ -12,6 +12,20 @@
 
 volatile const __u32 target_cpu_idx; /* This bpf object instance handles this core */
 
+static inline __u64 
+read_perf_event_counter(
+        size_t perf_event_type,
+        size_t cpu_idx)
+{
+    __u32 key = cpu_idx * NUM_EVENT_TYPES + perf_event_type;
+    struct bpf_perf_event_value value = {};
+
+    if (bpf_perf_event_read_value(&perf_event_map, key, &value, sizeof(value)) < 0)
+        return 0;
+
+    return value.counter;
+}
+
 SEC("iter/bpf_map_elem")
 int dump_counters(struct bpf_iter__bpf_map_elem* context)
 {
@@ -27,9 +41,13 @@ int dump_counters(struct bpf_iter__bpf_map_elem* context)
     if (!counters)
         return 0;
     
-    BPF_SEQ_PRINTF(seq, "%llu,%llu\n", 
+    BPF_SEQ_PRINTF(seq, "%llu,%llu,%ld,%ld,%ld,%ld\n", 
         counters->total_packets_received,
-        counters->total_rx_bytes_received);
+        counters->total_rx_bytes_received,
+        read_perf_event_counter(INSTRUCTIONS, target_cpu_idx),
+        read_perf_event_counter(CPU_CYCLES, target_cpu_idx),
+        read_perf_event_counter(REF_CPU_CYCLES, target_cpu_idx),
+        read_perf_event_counter(CACHE_MISSES, target_cpu_idx));
     
     return 0;
 }
