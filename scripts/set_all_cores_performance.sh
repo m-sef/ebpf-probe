1900b55662d42e5f9d7d6d68ab2575a8abf3e8bd
m-sef <seth.l.moore0506@gmail.com>
2026-05-29 09:45:16 -0400

    fix missing bash interpreter in set_all_cores_performance.sh shebang

--- scripts/set_all_cores_performance.sh
+++ scripts/set_all_cores_performance.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env
-for ((i=0; i<$(nproc); i++)); do sudo cpufreq-set --cpu $i --governor performance; done  
+#!/usr/bin/env bash
+for ((i=0; i<$(nproc); i++)); do sudo cpufreq-set --cpu $i --governor performance; done 
