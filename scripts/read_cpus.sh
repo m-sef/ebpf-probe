5a62abdb7b0f07865bc345a43a07f3c7d2e88cc7
Seth Moore <seth.l.moore0506@gmail.com>
2026-06-09 23:08:58 -0400

    Modified scripts

---
+++ scripts/read_cpus.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Usage: read_cpus LOG_FILE_PATH FREQUENCY
+# Brief: Intended to be piped/redirected to file
+
+LOG_FILE_PATH=$1
+FREQUENCY=$2
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 LOG_FILE_PATH FREQUENCY"
+    exit 1
+fi
+
+while true
+do
+    cat /sys/fs/bpf/ebpf_probe/cpu*/summary >> $LOG_FILE_PATH
+
+    sleep $FREQUENCY
+done
+
+exit 0
